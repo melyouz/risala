c902d65ff3b1d7bfd62c38dbee76def789a3a2e3
Mohammadi El Youzghi <mo.elyouzghi@gmail.com>
2024-10-29 22:22:26 +0100

    Add --with-sample-data flag

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -5,12 +5,13 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 
 	"github.com/go-chi/chi/v5"
 	_ "github.com/go-playground/validator/v10"
 
+	"github.com/melyouz/risala/broker/internal"
 	"github.com/melyouz/risala/broker/internal/http/server"
 	"github.com/melyouz/risala/broker/internal/sample"
 	"github.com/melyouz/risala/broker/internal/storage"
@@ -19,10 +20,22 @@ import (
 func main() {
 	listenAddr := "localhost:8000"
 	router := chi.NewRouter()
-	queueRepository := storage.NewInMemoryQueueRepository(sample.Queues)
-	exchangeRepository := storage.NewInMemoryExchangeRepository(sample.Exchanges)
+
+	withSampleData := flag.Bool("with-sample-data", false, "Initialize API with sample data")
+	flag.Parse()
+
+	queues := map[string]*internal.Queue{}
+	exchanges := map[string]*internal.Exchange{}
+	if *withSampleData {
+		queues = sample.Queues
+		exchanges = sample.Exchanges
+	}
+
+	queueRepository := storage.NewInMemoryQueueRepository(queues)
+	exchangeRepository := storage.NewInMemoryExchangeRepository(exchanges)
 
 	s := server.NewServer(listenAddr, router, queueRepository, exchangeRepository)
-	fmt.Printf("Listening on: http://%s\n", listenAddr)
+	log.Printf("Listening on: http://%s\n", listenAddr)
+	log.Printf("With sample data: %v", *withSampleData)
 	log.Fatal(s.ListenAndServe())
 }
--- broker/internal/sample/sample.go
+++ broker/internal/sample/sample.go
@@ -5,15 +5,33 @@
 package sample
 
 import (
+	"github.com/google/uuid"
+
 	"github.com/melyouz/risala/broker/internal"
 )
 
 var Exchanges = map[string]*internal.Exchange{
-	"app.internal": {Name: "app.internal", Bindings: []*internal.Binding{}},
-	"app.external": {Name: "app.external", Bindings: []*internal.Binding{}},
+	"app.internal": {Name: "app.internal", Bindings: []*internal.Binding{
+		{Id: uuid.New(), Queue: "events", RoutingKey: "#"},
+	}},
+	"app.external": {Name: "app.external", Bindings: []*internal.Binding{
+		{Id: uuid.New(), Queue: "tmp", RoutingKey: "#"},
+	}},
 }
 
 var Queues = map[string]*internal.Queue{
-	"events": {Name: "events", Durability: internal.Durability.DURABLE, Messages: []*internal.Message{}},
-	"tmp":    {Name: "tmp", Durability: internal.Durability.TRANSIENT, Messages: []*internal.Message{}},
+	"events": {Name: "events", Durability: internal.Durability.DURABLE, Messages: []*internal.Message{
+		{Id: uuid.New(), Payload: "Message 1 (events)"},
+		{Id: uuid.New(), Payload: "Message 2 (events)"},
+		{Id: uuid.New(), Payload: "Message 3 (events)"},
+		{Id: uuid.New(), Payload: "Message 4 (events)"},
+		{Id: uuid.New(), Payload: "Message 5 (events)"},
+	}},
+	"tmp": {Name: "tmp", Durability: internal.Durability.TRANSIENT, Messages: []*internal.Message{
+		{Id: uuid.New(), Payload: "Message 1 (tmp)"},
+		{Id: uuid.New(), Payload: "Message 2 (tmp)"},
+		{Id: uuid.New(), Payload: "Message 3 (tmp)"},
+		{Id: uuid.New(), Payload: "Message 4 (tmp)"},
+		{Id: uuid.New(), Payload: "Message 5 (tmp)"},
+	}},
 }
